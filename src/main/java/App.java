5c369b2ecee7c18eee3605faa682044db59aaa7c
manuel.salgado <manuel.salgado@king.com>
2019-04-12 19:52:27 +0200

    use custom filter endpoint + run in docker

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 // (C) king.com Ltd 2019
 
-import com.twitter.hbc.core.endpoint.RawEndpoint;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.endpoint.StreamingEndpoint;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 
 import java.io.Serializable;
+import java.util.Collections;
 
 /**
  * Usage [--twitter-source.consumerKey <key> --twitter-source.consumerSecret <secret> --twitter-source.token <token> --twitter-source.tokenSecret <tokenSecret>]
@@ -27,7 +28,7 @@ public class App {
                 params.has(TwitterSource.TOKEN) &&
                 params.has(TwitterSource.TOKEN_SECRET)) {
             final TwitterSource twitterSource = new TwitterSource(params.getProperties());
-            //twitterSource.setCustomEndpointInitializer(new CustomEndPoint());
+            twitterSource.setCustomEndpointInitializer(new CustomEndPoint());
             streamSource = env.addSource(twitterSource);
         } else {
             System.out.println("missing params");
@@ -38,11 +39,12 @@ public class App {
     }
 
 
-    //TODO pending to get it working with custom endpoint to filter by hashtag
     public static class CustomEndPoint implements TwitterSource.EndpointInitializer, Serializable {
         @Override
         public StreamingEndpoint createEndpoint() {
-            return new RawEndpoint("https://stream.twitter.com/1.1/statuses/filter.json?track=%23ServicioPublico", "POST");
+            StatusesFilterEndpoint streamingEndpoint = new StatusesFilterEndpoint();
+            streamingEndpoint.trackTerms(Collections.singletonList("#cars"));
+            return streamingEndpoint;
         }}
 
 }
